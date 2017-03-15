dff5bf0bd0ef733b506c9317247d89b597d496a5
Carlos Doki <carlosdoki@Carloss-MacBook-Pro.local>
2017-03-14 21:54:14 -0300

    Final

--- Scribe/ViewController.swift
+++ Scribe/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import Speech
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioPlayerDelegate{
+    
+    @IBOutlet weak var activitySpinner: UIActivityIndicatorView!
+    @IBOutlet weak var transcritionTextField: UITextView!
+    
+    var audioPlayer : AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        activitySpinner.isHidden = true
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        player.stop()
+        activitySpinner.stopAnimating()
+        activitySpinner.isHidden = true
+    }
+    
+    func requestSpeechAuth() {
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in
+            if authStatus == SFSpeechRecognizerAuthorizationStatus.authorized {
+                if let path = Bundle.main.url(forResource: "test", withExtension: "m4a") {
+                    do {
+                        let sound = try AVAudioPlayer(contentsOf: path)
+                        self.audioPlayer = sound
+                        self.audioPlayer.delegate = self
+                        sound.play()
+                    } catch {
+                        print("Error!")
+                    }
+                    
+                    let reconizer = SFSpeechRecognizer()
+                    let request = SFSpeechURLRecognitionRequest(url: path)
+                    reconizer?.recognitionTask(with: request) { (result, error) in
+                        if let error = error {
+                            print("There was an error: \(error)")
+                        } else {
+                            self.transcritionTextField.text = result?.bestTranscription.formattedString
+                        }
+                    }
+                }
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func playButtonPressed(_ sender: CircleButton) {
+        activitySpinner.isHidden = false
+        activitySpinner.startAnimating()
+        requestSpeechAuth()
     }
-
-
 }
 
