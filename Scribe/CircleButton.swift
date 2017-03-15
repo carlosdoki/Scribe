19c001c931a1e8f464fc68c66380479639af4e37
Carlos Doki <carlosdoki@Carloss-MacBook-Pro.local>
2017-03-14 21:53:25 -0300

    Final

---
+++ Scribe/CircleButton.swift
@@ -0,0 +1,21 @@
+//
+//  CircleButton.swift
+//  Scribe
+//
+//  Created by Carlos Doki on 14/03/17.
+//  Copyright © 2017 Carlos Doki. All rights reserved.
+//
+
+import UIKit
+
+class CircleButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
